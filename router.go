f3d3790001eec930a5c724795de7856d4c1cb808
17847869 <shibkikh.i.v@sberbank.ru>
2024-07-17 17:21:01 +0300

    remove debug text from readme

--- router.go
+++ router.go
@@ -83,3 +83,36 @@ func catch(next types.Handler) http.HandlerFunc {
 		}
 	}
 }
+
+/*
+HTTP/1.1 200 OK
+Content-Encoding: gzip
+Content-Type: text/html; charset=utf-8
+Date: Sun, 16 Jun 2024 09:56:18 GMT
+ETag: W/"34d80-RRdtGW1ieWo+bM5DRkLbeL33cQQ"
+Server: Tank
+Strict-Transport-Security: max-age=31536000; includeSubDomains
+Transfer-Encoding: Identity
+Vary: Accept-Encoding, Accept-Encoding
+X-Content-Type-Options: nosniff
+X-DNS-Prefetch-Control: off
+X-Download-Options: noopen
+X-Frame-Options: SAMEORIGIN
+X-Request-Detected-Device: desktop
+X-Request-Geoip-Country-Code: RU
+X-Request-Id: fd447a85a55d4d16944b656fc2cea2de
+X-XSS-Protection: 1; mode=block
+
+HTTP/1.1 200 OK
+Content-Type: text/plain
+Transfer-Encoding: chunked
+
+0\r\n
+Mozilla\r\n
+7\r\n
+Developer\r\n
+9\r\n
+Network\r\n
+0\r\n
+\r\n
+*/
