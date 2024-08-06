38604416fe2bd258fe85ba289e5e39298774f8d3
17847869 <shibkikh.i.v@sberbank.ru>
2024-08-06 21:20:48 +0300

    tmp change

--- middleware/real_ip.go
+++ middleware/real_ip.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -11,7 +12,10 @@ func RealIP(next types.Handler) types.Handler {
 	return func(w http.ResponseWriter, r *http.Request) error {
 		ip := proxiedIP(r.Header)
 		if ip != "" {
+			fmt.Println("RemoteAddr:", ip)
 			r.RemoteAddr = ip
+		} else {
+			fmt.Println("ip not changed:", ip)
 		}
 		return next(w, r)
 	}
@@ -20,9 +24,10 @@ func RealIP(next types.Handler) types.Handler {
 func proxiedIP(header http.Header) string {
 	ip := header.Get(types.XForwardedFor)
 	if ip != "" {
-		i := strings.LastIndex(ip, ",")
+		fmt.Println("XForwardedFor:", ip)
+		i := strings.Index(ip, ",")
 		if i > -1 {
-			ip = strings.TrimSpace(ip[i+1:])
+			ip = strings.TrimSpace(ip[:i])
 			return ip
 		}
 	}
