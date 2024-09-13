35e7e3d46847d2525011a545481debe12838b16a
17847869 <shibkikh.i.v@sberbank.ru>
2024-09-13 09:36:48 +0300

    fix logging issue

--- logger_func.go
+++ logger_func.go
@@ -12,6 +12,7 @@ import (
 func defaultLoggerFunc(w *types.ResponseWrapper, r *http.Request, err error) {
 	if err != nil {
 		slog.Error(strconv.Itoa(w.Status()), "error", err, "trace-id", r.Header.Get(types.XRequestId))
+		handleError(w, err)
 	}
 
 	slog.Info("access",
