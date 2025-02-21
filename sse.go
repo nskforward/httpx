b8881eec1a22f9fec107374edef0484656c1eea6
Ivan Shibkikh <ivan.shib@gmail.com>
2025-02-21 21:48:16 +0300

    improve sse

--- sse.go
+++ sse.go
@@ -1,50 +1,117 @@
 package httpx
 
-import "time"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"sync/atomic"
+	"time"
+)
 
-type sseSender func(
-	send func(name, value string),
-	flush func(),
-) bool
+var (
+	ErrSSEventStreamClosed = fmt.Errorf("stream cloased")
+)
 
-// Stream returns FALSE if client gone, TRUE if server breaks stream.
-func (ctx *Ctx) Stream(step sseSender) bool {
-	ctx.SetHeader("Content-Type", "text/event-stream")
-	ctx.SetHeader("Cache-Control", "no-store")
+type SSEventStream struct {
+	w          http.ResponseWriter
+	buf        bytes.Buffer
+	controller *http.ResponseController
+	encoder    *json.Encoder
+	active     atomic.Bool
+	intput     chan SSEvent
+	ctx        context.Context
+	cancel     context.CancelFunc
+}
 
-	gone := ctx.Request().Context().Done()
-	writer := ctx.w
-	flusher := ctx.w.Flusher()
+type SSEvent struct {
+	Type    string `json:"type"`
+	Payload any    `json:"payload"`
+}
 
-	send := func(name, value string) {
-		writer.Write([]byte(name))
-		writer.Write([]byte(": "))
-		writer.Write([]byte(value))
-		writer.Write([]byte{'\n'})
+func NewSSEventStream(w http.ResponseWriter, r *http.Request) *SSEventStream {
+	ctx, cancel := context.WithCancel(r.Context())
+
+	s := &SSEventStream{
+		w:          w,
+		controller: http.NewResponseController(w),
+		encoder:    json.NewEncoder(w),
+		intput:     make(chan SSEvent, 4),
+		ctx:        ctx,
+		cancel:     cancel,
 	}
 
-	flush := func() {
-		writer.Write([]byte{'\n'})
-		flusher.Flush()
-	}
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-store")
 
+	go s.loop()
+	return s
+}
+
+func (s *SSEventStream) Close() {
+	s.active.Store(false)
+	s.cancel()
+	close(s.intput)
+}
+
+func (s *SSEventStream) Next() bool {
+	return s.active.Load()
+}
+
+func (s *SSEventStream) Send(event SSEvent) error {
+	if !s.active.Load() {
+		return ErrSSEventStreamClosed
+	}
+	s.intput <- event
+	return nil
+}
+
+func (s *SSEventStream) loop() {
 	ticker := time.NewTicker(15 * time.Second)
 	defer ticker.Stop()
 
+	defer s.active.Store(false)
+
 	for {
 		select {
-		case <-gone:
-			return false
+		case <-s.ctx.Done():
+			return
 
 		case <-ticker.C:
-			writer.Write([]byte(":ping\n"))
-			flusher.Flush()
+			err := s.ping()
+			if err != nil {
+				return
+			}
 
-		default:
-			keepOpen := step(send, flush)
-			if !keepOpen {
-				return true
+		case event := <-s.intput:
+			err := s.write(event)
+			if err != nil {
+				return
 			}
 		}
 	}
+}
+
+func (s *SSEventStream) ping() error {
+	_, err := s.w.Write([]byte(":ping\n"))
+	if err != nil {
+		return err
+	}
+	return s.controller.Flush()
+}
+
+func (s *SSEventStream) write(event SSEvent) error {
+	s.buf.WriteString("data: ")
+	err := s.encoder.Encode(event)
+	if err != nil {
+		return err
+	}
+	s.buf.WriteString("\n\n")
+	_, err = io.Copy(s.w, &s.buf)
+	if err != nil {
+		return err
+	}
+	return s.controller.Flush()
 }
