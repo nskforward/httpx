40d0f2ceee8881434923c6ab90ea734f17310342
Ivan <Shibkikh.I.V@sberbank.ru>
2025-07-25 16:29:14 +0300

    correct types

--- types.go
+++ types.go
@@ -3,6 +3,6 @@ package httpx
 import "net/http"
 
 type Multiplexer interface {
-	HandleFunc(pattern string, handler http.HandlerFunc)
+	HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request))
 	Handle(pattern string, handler http.Handler)
 }
